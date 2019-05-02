9e8eb1cdd2dc30b4109f4838101185bada60196b
MaheKarim <mahekarim@gmail.com>
2019-05-02 16:38:29 +0600

    blade templating full

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,9 +6,15 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-  
+
     public function index(){
 
         return view('front-end.home');
     }
+    public function single(){
+
+        return view('front-end.single');
+    }
+
+
 }
