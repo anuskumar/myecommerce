6c577860c635de5fce281be0417f3523e55283fc
MaheKarim <mahekarim@gmail.com>
2019-05-12 01:40:10 +0600

    Product Table Seeder

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -9,18 +9,18 @@ class ProductTableSeeder extends Seeder
 
     public function run()
     {
-       // $products= ['Hotel Management','School Management'];
-        for ($i=1; $i <= 100 ; $i++){
+        // $products= ['Hotel Management','School Management'];
+        for ($i = 1; $i <= 100; $i++) {
             Product::create([
-                'category_id' => rand(52,54),
-                'name' => 'Product' .$i,
-                'slug' => str_slug('Product' .$i),
+                'category_id' => rand(52, 54),
+                'name' => 'Product' . $i,
+                'slug' => str_slug('Product' . $i),
                 'description' => str_random(120),
-                'quantity' => rand(1,10),
-                'price' => rand(100,5000),
+                'quantity' => rand(1, 10),
+                'price' => rand(100, 5000),
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now(),
-                ]);
+            ]);
         }
     }
 }
