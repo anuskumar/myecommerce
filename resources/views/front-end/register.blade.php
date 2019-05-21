1aa7ff493496a54235ae4f12e3c0845bd58bf5b0
MaheKarim <mahekarim@gmail.com>
2019-05-22 03:42:40 +0600

    phn_number add

--- resources/views/front-end/register.blade.php
+++ resources/views/front-end/register.blade.php
@@ -9,11 +9,23 @@
     <br>
     <div class="card mt-4">
         <form>
+
             <div class="form-group">
-              <label for="exampleInputEmail1">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+              <label for="full_name">Full Name</label>
+              <input type="text" name="full_name" class="form-control" id="full_name" placeholder="Full Name">
             </div>
+
+
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" name="email" class="form-control" id="email" placeholder="Enter email">
+            </div>
+
+            <div class="form-group">
+              <label for="phn_number">PHN Number</label>
+              <input type="text" name="phn_number" class="form-control" id="phn_number" placeholder="PHN Number">
+            </div>
+
             <div class="form-group">
               <label for="exampleInputPassword1">Password</label>
               <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
